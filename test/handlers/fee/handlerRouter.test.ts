772f8859d448a004a9d356064365854ed8317dcd
nmlinaric <nikola.mlinaric93@gmail.com>
2023-10-17 11:36:49 +0200

    Keep fee handler whitelisting logic

--- test/handlers/fee/handlerRouter.test.ts
+++ test/handlers/fee/handlerRouter.test.ts
@@ -4,9 +4,9 @@
 import { assert, expect } from "chai";
 import { ethers } from "hardhat";
 import type { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
-import { deployBridge, createResourceID } from "../../helpers";
+import { createResourceID, createERCDepositData } from "../../helpers";
 import type {
-  Bridge,
+  BasicFeeHandler,
   ERC20PresetMinterPauser,
   FeeHandlerRouter,
 } from "../../../typechain-types";
@@ -14,19 +14,31 @@ import type {
 describe("FeeHandlerRouter", () => {
   const originDomainID = 1;
   const destinationDomainID = 2;
+  const feeData = "0x";
 
-  let bridgeInstance: Bridge;
   let ERC20MintableInstance: ERC20PresetMinterPauser;
   let feeHandlerRouterInstance: FeeHandlerRouter;
+  let basicFeeHandlerInstance: BasicFeeHandler;
   let feeHandlerAccount: HardhatEthersSigner;
   let nonAdminAccount: HardhatEthersSigner;
+  let whitelistedAccount: HardhatEthersSigner;
+  let nonWhitelistedAccount: HardhatEthersSigner;
+  let recipientAccount: HardhatEthersSigner;
+  let bridgeInstance: HardhatEthersSigner;
 
   let resourceID: string;
 
   beforeEach(async () => {
-    [, feeHandlerAccount, nonAdminAccount] = await ethers.getSigners();
+    [
+      ,
+      recipientAccount,
+      feeHandlerAccount,
+      nonAdminAccount,
+      whitelistedAccount,
+      nonWhitelistedAccount,
+      bridgeInstance,
+    ] = await ethers.getSigners();
 
-    bridgeInstance = await deployBridge(originDomainID);
     const ERC20MintableContract = await ethers.getContractFactory(
       "ERC20PresetMinterPauser",
     );
@@ -36,7 +48,12 @@ describe("FeeHandlerRouter", () => {
     feeHandlerRouterInstance = await FeeHandlerRouterContract.deploy(
       await bridgeInstance.getAddress(),
     );
-
+    const BasicFeeHandlerContract =
+      await ethers.getContractFactory("BasicFeeHandler");
+    basicFeeHandlerInstance = await BasicFeeHandlerContract.deploy(
+      await bridgeInstance.getAddress(),
+      await feeHandlerRouterInstance.getAddress(),
+    );
     resourceID = createResourceID(
       await ERC20MintableInstance.getAddress(),
       originDomainID,
@@ -74,5 +91,161 @@ describe("FeeHandlerRouter", () => {
           feeHandlerAccount.getAddress(),
         ),
     ).to.be.revertedWith("sender doesn't have admin role");
+  });
+
+  it("should successfully set whitelist on an address", async () => {
+    assert.equal(
+      await feeHandlerRouterInstance._whitelist(
+        await whitelistedAccount.getAddress(),
+      ),
+      false,
+    );
+
+    const whitelistTx = await feeHandlerRouterInstance.adminSetWhitelist(
+      await whitelistedAccount.getAddress(),
+      true,
+    );
+    assert.equal(
+      await feeHandlerRouterInstance._whitelist(
+        await whitelistedAccount.getAddress(),
+      ),
+      true,
+    );
+    await expect(whitelistTx)
+      .to.emit(feeHandlerRouterInstance, "WhitelistChanged")
+      .withArgs(await whitelistedAccount.getAddress(), true);
+  });
+
+  it("should require admin role to set whitelist address", async () => {
+    await expect(
+      feeHandlerRouterInstance
+        .connect(nonAdminAccount)
+        .adminSetWhitelist(await nonWhitelistedAccount.getAddress(), true),
+    ).to.be.revertedWith("sender doesn't have admin role");
+  });
+
+  it("should return fee 0 if address whitelisted", async () => {
+    await feeHandlerRouterInstance.adminSetWhitelist(
+      await whitelistedAccount.getAddress(),
+      true,
+    );
+    await feeHandlerRouterInstance.adminSetResourceHandler(
+      destinationDomainID,
+      resourceID,
+      await basicFeeHandlerInstance.getAddress(),
+    );
+    await basicFeeHandlerInstance.changeFee(ethers.parseEther("0.5"));
+
+    const depositData = createERCDepositData(
+      100,
+      20,
+      await recipientAccount.getAddress(),
+    );
+    const { fee: feeBefore } = await feeHandlerRouterInstance.calculateFee(
+      await whitelistedAccount.getAddress(),
+      originDomainID,
+      destinationDomainID,
+      resourceID,
+      depositData,
+      feeData,
+    );
+    assert.equal(feeBefore, BigInt(0));
+    const { fee: feeAfter } = await feeHandlerRouterInstance.calculateFee(
+      await nonWhitelistedAccount.getAddress(),
+      originDomainID,
+      destinationDomainID,
+      resourceID,
+      depositData,
+      feeData,
+    );
+    assert.equal(feeAfter, ethers.parseEther("0.5"));
+  });
+
+  it("should revert if whitelisted address provides fee", async () => {
+    await feeHandlerRouterInstance.adminSetWhitelist(
+      await whitelistedAccount.getAddress(),
+      true,
+    );
+    await feeHandlerRouterInstance.adminSetResourceHandler(
+      destinationDomainID,
+      resourceID,
+      await basicFeeHandlerInstance.getAddress(),
+    );
+    await basicFeeHandlerInstance.changeFee(ethers.parseEther("0.5"));
+
+    const depositData = createERCDepositData(
+      100,
+      20,
+      await recipientAccount.getAddress(),
+    );
+    await expect(
+      feeHandlerRouterInstance
+        .connect(bridgeInstance)
+        .collectFee(
+          await whitelistedAccount.getAddress(),
+          originDomainID,
+          destinationDomainID,
+          resourceID,
+          depositData,
+          feeData,
+          {
+            value: ethers.parseEther("0.5"),
+          },
+        ),
+    ).to.be.revertedWithCustomError(
+      feeHandlerRouterInstance,
+      "IncorrectFeeSupplied(uint256)",
+    );
+
+    await expect(
+      feeHandlerRouterInstance
+        .connect(bridgeInstance)
+        .collectFee(
+          nonWhitelistedAccount.getAddress(),
+          originDomainID,
+          destinationDomainID,
+          resourceID,
+          depositData,
+          feeData,
+          {
+            value: ethers.parseEther("0.5"),
+          },
+        ),
+    ).not.to.be.reverted;
+  });
+
+  it("should not collect fee from whitelisted address", async () => {
+    await feeHandlerRouterInstance.adminSetWhitelist(
+      await whitelistedAccount.getAddress(),
+      true,
+    );
+    await feeHandlerRouterInstance.adminSetResourceHandler(
+      destinationDomainID,
+      resourceID,
+      await basicFeeHandlerInstance.getAddress(),
+    );
+    await basicFeeHandlerInstance.changeFee(ethers.parseEther("0.5"));
+
+    const depositData = createERCDepositData(
+      100,
+      20,
+      await recipientAccount.getAddress(),
+    );
+    await expect(
+      feeHandlerRouterInstance
+        .connect(bridgeInstance)
+        .collectFee(
+          await whitelistedAccount.getAddress(),
+          originDomainID,
+          destinationDomainID,
+          resourceID,
+          depositData,
+          feeData,
+          {
+            from: await bridgeInstance.getAddress(),
+            value: "0",
+          },
+        ),
+    ).not.to.be.reverted;
   });
 });
