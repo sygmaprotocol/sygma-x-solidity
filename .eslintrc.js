9f1d54c332514b2dd9a6309b30fb6662e4700497
nmlinaric <nikola.mlinaric93@gmail.com>
2023-10-23 15:07:50 +0200

    Add linting to ci

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,6 @@ module.exports = {
     "@typescript-eslint/no-unsafe-member-access": 0,
     "@typescript-eslint/no-unsafe-call": 0,
     "@typescript-eslint/no-unsafe-assignment": 0,
+    "@typescript-eslint/no-unsafe-argument": 0,
   }
 }
