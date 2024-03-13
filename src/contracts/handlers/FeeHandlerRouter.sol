8342d3d537f87979d4dd4cc800cd84e4d1489bb9
Nikola Mlinarić <35880252+nmlinaric@users.noreply.github.com>
2024-03-13 14:26:24 +0100

    chore: replace custom AccessControl implementation with OZ's (#34)

--- src/contracts/handlers/FeeHandlerRouter.sol
+++ src/contracts/handlers/FeeHandlerRouter.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.11;
 
 import "../interfaces/IFeeHandler.sol";
-import "../utils/AccessControl.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 /**
     @title Handles FeeHandler routing for resources.
